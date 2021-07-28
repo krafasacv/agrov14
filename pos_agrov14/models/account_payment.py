336202652e363ec46ca9cb7e2ad4a372e311c1af
krafasacv <43054099+krafasacv@users.noreply.github.com>
2021-07-27 22:57:07 -0500

    Update account_payment.py

--- pos_agrov14/models/account_payment.py
+++ pos_agrov14/models/account_payment.py
@@ -3,7 +3,9 @@ from odoo import models, fields, api
 class PriceListMin(models.Model):
     _inherit = 'account.payment'
     x_monto_mn = fields.Char('Monto en Letra 00/100 MN', compute="_montomn", store=False)
-
+    x_forma_de_pago = fields.Selection(selection = [('transferencia', 'Transferencia'), ('deposito', 'Deposito'),('cheque','Cheque'),], string =('Forma de pago'), store=True)
+    x_folio_seguimiento = fields.Char('Folio de seguimiento', store=True)
+    
     @api.depends('state')
     def _montomn(self):
         monto = self.amount
@@ -12,3 +14,9 @@ class PriceListMin(models.Model):
                 int(round(monto - int(monto), 2) * 100)) + '/100 M.N.'
         else:
             self['x_monto_mn'] = 'CHEQUE CANCELADO'
+
+            
+class AccountPaymentRegister(models.TransientModel):
+    _inherit = 'account.payment.register'
+    x_forma_de_pago = fields.Selection(selection = [('transferencia', 'Transferencia'), ('deposito', 'Deposito'),('cheque','Cheque'),], string =('Forma de pago'), store=True)
+    x_folio_seguimiento = fields.Char('Folio de seguimiento', store=True)
