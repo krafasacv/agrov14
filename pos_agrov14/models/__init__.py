ca71045e92571c344a6eed6751b1d15a0ab52dd3
krafasacv <43054099+krafasacv@users.noreply.github.com>
2021-08-06 19:32:20 -0500

    Update __init__.py

--- pos_agrov14/models/__init__.py
+++ pos_agrov14/models/__init__.py
@@ -1,1 +1,1 @@
-from . import models, pos_order_to_invoice, nv_to_pos_invoice, sale_order, account_payment, res_partner
+from . import models, pos_order_to_invoice, nv_to_pos_invoice, sale_order, account_payment
