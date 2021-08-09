f8d2bde9d65640fb5227a312aa00c6e07dd55760
krafasacv <43054099+krafasacv@users.noreply.github.com>
2021-08-09 18:27:39 -0500

    Add files via upload

---
+++ pos_agrov14/models/kardex.py
@@ -0,0 +1,8 @@
+from odoo import models, fields, api
+
+class PriceListMin(models.Model):
+    _inherit = 'stock.move.line'
+    x_costo_generado = fields.Float(string='Costo', related='move_id.price_unit')
+
+
+
