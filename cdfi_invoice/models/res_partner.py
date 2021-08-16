c3751a101a4c891491a0e27588ec95285d858585
krafasacv <43054099+krafasacv@users.noreply.github.com>
2021-08-15 22:52:33 -0500

    Update res_partner.py

--- cdfi_invoice/models/res_partner.py
+++ cdfi_invoice/models/res_partner.py
@@ -5,7 +5,6 @@ from odoo import fields, models, _
 class ResPartner(models.Model):
     _inherit = 'res.partner'
     
-    x_cumple = fields.Char(string=_('cumple'), store=True)
     residencia_fiscal = fields.Char(string=_('Residencia Fiscal'))
     registro_tributario = fields.Char(string=_('Registro tributario'))
     uso_cfdi = fields.Selection(
