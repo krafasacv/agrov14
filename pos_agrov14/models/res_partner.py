45b7576180800e1c4442b36687db645a07b80f5b
krafasacv <43054099+krafasacv@users.noreply.github.com>
2021-08-15 22:08:46 -0500

    Update res_partner.py

--- pos_agrov14/models/res_partner.py
+++ pos_agrov14/models/res_partner.py
@@ -3,7 +3,7 @@ from odoo import models, fields, api
 class PriceListMin(models.Model):
     _inherit = 'res.partner'
     
-    x_birthday = fields.Date('Fecha de Cumpleaños',store=True)
+    
     
     x_cumple = fields.Char('cumple', store=True)
     
