f224d07804e089120ad1fcb5259e085268ff73f6
krafasacv <43054099+krafasacv@users.noreply.github.com>
2021-08-16 21:41:16 -0500

    Add files via upload

---
+++ agrov14/__manifest__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "POS AGROMORALES",
+    'summary': "Modificaciones a res.partner",
+    'description': """Agrega campos al res.partner """,
+    'author': "KRA-FA SA DE CV",
+    'website': "http://www.krafa.com.mx",
+    'category': 'Partner',
+    'version': '14.0.1',
+    'depends': ['point_of_sale'],
+}
