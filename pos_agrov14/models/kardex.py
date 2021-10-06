7f90d271560b5cd36bc7062a1534e43d745ce855
krafasacv <43054099+krafasacv@users.noreply.github.com>
2021-10-05 19:41:22 -0500

    Update kardex.py

--- pos_agrov14/models/kardex.py
+++ pos_agrov14/models/kardex.py
@@ -3,6 +3,7 @@ from odoo import models, fields, api
 class PriceListMin(models.Model):
     _inherit = 'stock.move.line'
     x_costo_generado = fields.Float(string='Costo', related='move_id.price_unit')
+    x_origen = fields.Integer(string='Origen', related='move_id.location_id.id', readonly = True, storage= True)
 
 
 
