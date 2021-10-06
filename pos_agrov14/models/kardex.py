c5af8b8a887b698a1574c5df28855d09ef723556
krafasacv <43054099+krafasacv@users.noreply.github.com>
2021-10-05 19:46:22 -0500

    Update kardex.py

--- pos_agrov14/models/kardex.py
+++ pos_agrov14/models/kardex.py
@@ -5,3 +5,4 @@ class PriceListMin(models.Model):
     x_costo_generado = fields.Float(string='Costo', related='move_id.price_unit')
     x_origen = fields.Integer(string='Origen', related='move_id.location_id.id', readonly = True, storage= True)
     x_entrada_salida = fields.Char(string='Entrada/Salida', readonly = True)
+    x_compra = fields.Char(string='Compra', related = 'move_id.purchase_line_id.order_id.x_studio_nota_entrada', readonly = True, storage = True)
